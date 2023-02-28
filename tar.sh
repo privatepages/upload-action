9e3e4c5091d81c8d21d4adb85bd9c0f9231f97f0
hell <helldweller@protonmail.com>
2023-02-28 21:35:38 +0700

    example sh

---
+++ tar.sh
@@ -0,0 +1,18 @@
+# WIP
+
+# only for example 
+
+# artifact will be placed there
+# $1 <SUBDIR>
+# $2 privatepages URL
+# $3 artifact name
+# $4 token
+pwd # current dir is project root
+
+tar -cvzf artifact.tar.gz -C $1 .
+
+curl -X POST https://$2/upload \
+    -F "file=@$(realpath artifact.tar.gz)" \
+    -H "Content-Type: multipart/form-data" \
+    -F "artifactname=$3" \
+    -F "token=$4"
